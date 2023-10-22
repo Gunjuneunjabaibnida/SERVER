a6bc793597dfdef6ac7aa1733f0ea88859d9542e
Gunjuneunjabaibnida <148675252+Gunjuneunjabaibnida@users.noreply.github.com>
2023-10-22 21:33:55 +0700

    Update server.php

--- server.php
+++ server.php
@@ -1,17 +1,21 @@
 function server(){
-  system('termux-open https://www.highcpmrevenuegate.com/k99xk5vp03?key=d61cbbf2439c9dbd81a43bf04c8dec95');
   $r =  str_repeat("\033[1;95m~", 53)."\n";
   pil:
-    print "\033[1;92m1. Claimbits      => https://tutwuri.id/HdikAH5\n";
-    print "\033[1;92m2. Claimfreecoins => https://tutwuri.id/zuLacYmL\n";
-    print "\033[1;92m3. Faucetpk       => https://tutwuri.id/1yNECie\n";
-    print "\033[1;92m4. Balkanofaucet  => https://tutwuri.id/k7nDp\n";
-    print "\033[1;92m5. Auto faucet    => https://tutwuri.id/Wtr89LY\n";
-    print "\033[1;92m6. Exit\n";
+    print "\033[1;92m1. Earnbitmoon      => \n";
+    print "\033[1;92m2. Faucetpayz       => https://tinyurl.com/5fsu3sv5 \n";
+    print "\033[1;92m3. Advies           => \n";
+    print "\033[1;92m4. Solpick          => \n";
+    print "\033[1;92m5. Tronpick         => \n";
+    print "\033[1;92m6. Dogepick         => \n";
+    print "\033[1;92m7. Topadcoin        => \n";
+    print "\033[1;92m8. Crypto Planet    => \n";
+    print "\033[1;92m9. Tronpick         => \n";
+    print "\033[1;92m10. Tronpick        => \n";
+    print "\033[1;92m11. Exit \n";
     $pil["pilih"]=readline("\033[1;97mInput Your Number : \033[1;92m");
     print"\033[1;95m$r";
     if($pil["pilih"]==1){
-      eval(file_get_contents('https://raw.githubusercontent.com/DavillScript/Server/main/Claimbits'));
+      eval(file_get_contents('https://github.com/Gunjuneunjabaibnida/SERVER/blob/main/Earnbitmoon'));
     }elseif($pil["pilih"]==2){
       eval(file_get_contents('https://raw.githubusercontent.com/DavillScript/Server/main/Claimfreecoins'));
     }elseif($pil["pilih"]==3){
