a230a2592de791f7e9bd411b6571cf52f80cf16b
Gunjuneunjabaibnida <148675252+Gunjuneunjabaibnida@users.noreply.github.com>
2023-10-24 23:48:38 +0700

    Update server.php

--- server.php
+++ server.php
@@ -2,10 +2,10 @@ function server(){
   $r =  str_repeat("\033[0;36m=", 53)."\n";
   pil:
     print "\033[1;37m1. Earnbitmoon \033[1;36m         \033[1;32mLink web -> \033[1;33mhttps://tinyurl.com/3jr7ss2f\n";
-    print "\033[1;37m2. Faucetpayz  \033[1;36m[apikey] \033[1;32mLink web -> \033[1;33mhttps://tinyurl.com/5fsu3sv5 \n";
-    print "\033[1;37m3. Advies      \033[1;36m[apikey] \033[1;32mLink web -> \033[1;33mhttps://tinyurl.com/y2svh9fh\n";
-    print "\033[1;37m4. Solpick     \033[1;36m[apikey] \033[1;32mLink web -> \033[1;33mhttps://tinyurl.com/59h4w63u\n";
-    print "\033[1;37m5. Tronpick    \033[1;36m[apikey] \033[1;32mLink web -> \033[1;33mhttps://tinyurl.com/tronpick\n";
+    print "\033[1;37m2. Faucetpayz  \033[1;36mapikey \033[1;32mLink web -> \033[1;33mhttps://tinyurl.com/5fsu3sv5 \n";
+    print "\033[1;37m3. Advies      \033[1;36mapikey \033[1;32mLink web -> \033[1;33mhttps://tinyurl.com/y2svh9fh\n";
+    print "\033[1;37m4. Solpick     \033[1;36mapikey \033[1;32mLink web -> \033[1;33mhttps://tinyurl.com/59h4w63u\n";
+    print "\033[1;37m5. Tronpick    \033[1;36mapikey \033[1;32mLink web -> \033[1;33mhttps://tinyurl.com/tronpick\n";
     print "\033[1;37m6. \033[1;31mExit \n";
     $pil["pilih"]=readline($putih."Input Your Number : \033[1;92m");
     print"\033[1;95m$r";
